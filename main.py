5a3d44bc558ea7cb7700d690284a8d41805560ce
dominikmolitor <dominik-jakob.molitor@protonmail.com>
2023-11-27 18:47:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from pyvis.network import Network
+import networkx as nx
+import streamlit as st
+import re
+from stvis import pv_static
+
+if __name__ == '__main__':
+    tk.Tk()
+
+    tk.mainloop()
